eecddb367259f8142145f020de6dd6f6dcda87c1
MJ Tucker <mjtucker08@gmail.com>
2023-09-12 22:51:21 -0400

    test commit

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className="title">
+        <h1 className="text-3xl font-bold underline text-red-700">
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
